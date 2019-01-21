9ca35ccf6aca9e24d1f40d13fb69af47cd3d2b41
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2019-01-21 09:35:01 +0100

    [1] Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6*/
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+  return (
+    <div className="ui container" style={{ marginTop: '10px'}}>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
